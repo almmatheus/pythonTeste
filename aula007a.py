6055444dd1072386fafed3ca09abb6c393d5509d
Matheus Almeida <98068423+almmatheus@users.noreply.github.com>
2023-06-27 11:44:14 -0300

    Add files via upload

---
+++ aula007a.py
@@ -0,0 +1,10 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+su = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di =  n1 // n2
+e = n1 ** n2
+print(f'A soma é {s}, a subtração é {su}, o produto é {m}')
+print(f'Divisão é {d:.3f}, divisão inteira é {di}, e potência é {e}')
