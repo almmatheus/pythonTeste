100ecd0a028ab7e71e94faf8d9ab32478fade328
Matheus Almeida <98068423+almmatheus@users.noreply.github.com>
2023-06-30 13:01:24 -0300

    Add files via upload

---
+++ aula009a.py
@@ -0,0 +1,36 @@
+frase = ('Curso em Vídeo Python')
+print(frase.count('o'))
+
+
+"""frase = ('Curso em Vídeo Python')
+print(frase[9])"""
+
+"""frase = ('Curso em Vídeo')
+print(frase[0:15])"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase[9:21:2])"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase[:14])"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase[10:])"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase[7::3])"""
+
+"""Welcome! Are you completely new to programming?
+about why and hot to get started with python. Fortunately
+an experienced programmer in any programming language
+(whatever it may be) can pick up Python very quickly.
+Its also very easy for beginners to use and learn, so jump in!"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase.__len__())"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase.__len__())"""
+
+"""frase = ('Curso em Vídeo Python')
+print(frase.upper().__len__('O'))"""
